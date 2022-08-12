b672b15f6c7a36767be80b6a561bf1e4c2681bc4
Gerardo Diosdado <diosescalera@gmail.com>
2022-08-12 13:00:51 -0500

    Binario en Reversa

--- Decimal_Binario.cpp
+++ Decimal_Binario.cpp
@@ -3,17 +3,44 @@
 
 using namespace std;
 
-int conversor(int decimal);
+int division(int decimal);
 
-int main()
-{
+int main(){
     int decimal = 0;
+    int binario = 1;
+
     cout << "Ingresa un numero decimal positivo: ";
     cin >> decimal;
-    cout << "El numero " << decimal << " es " << conversor(decimal) << " en binario" << endl;
+    cout << division(decimal) <<  endl;
+    //binario = division(decimal);
+    //cout << "El numero " << decimal << " es " << division(decimal) << " en binario" << endl;
     return 0;
 };
 
-int conversor(int decimal){
+int division(int decimal){
+    int residuo = 0;
+    int memoria = 1;
+    int contador = 0;
+
+    while (decimal > 1){
+        residuo = decimal % 2;
+        decimal = decimal / 2;
+        cout << "su mitad es " << decimal << " Y su residuo es " << residuo << endl;
+        if (residuo == 0)
+        {
+            memoria = memoria * 10;
+        }else{
+            memoria = (memoria * 10) + 1;
+        }
+        cout << "Memoria: " << memoria << endl; 
+    }
+    if (decimal > 0)
+    {
+        memoria = (memoria * 10) + 1;
+    }else{
+        memoria = 0;
+    }
     
+    
+    return memoria;
 }
